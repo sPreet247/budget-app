7b3f4602c595dfff7d77041e481f79d79ff06623
sPreet247 <samre2472@gmail.com>
2022-07-30 15:47:26 -0400

    final

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+
+import AddExpenseForm from "./components/AddExpenseForm";
+import Budget from "./components/Budget";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseTotal from "./components/ExpenseTotal";
+import RemainingBudget from "./components/Remaining";
+import { AppProvider } from "./context/AppContext";
+
+const App = () => {
+  return (
+    <AppProvider>
+      <div className="container">
+        <h1 className="mt-3">Budget App</h1>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <Budget />
+          </div>
+          <div className="col-sm">
+            <RemainingBudget />
+          </div>
+          <div className="col-sm">
+            <ExpenseTotal />
+          </div>
+        </div>
+        <h3 className="mt-3">Expenses</h3>
+        <div className="row ">
+          <div className="col-sm">
+            <ExpenseList />
+          </div>
+        </div>
+        <h3 className="mt-3">Add Expense</h3>
+        <div className="row mt-3">
+          <div className="col-sm">
+            <AddExpenseForm />
+          </div>
+        </div>
+      </div>
+    </AppProvider>
+  );
+};
+
+export default App;
